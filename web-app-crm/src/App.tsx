cb0325aba2967545efe08422122cbf4be0967a5f
GLiese-S <oleg-sultan@rambler.ru>
2022-07-22 18:00:12 +0800

    add web-app-crm

---
+++ web-app-crm/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { AuthPage } from './pages/auth-page';
+
+
+function App() {
+  return (
+    <div className="App">
+      <AuthPage />
+    </div>
+  );
+}
+
+export default App;
