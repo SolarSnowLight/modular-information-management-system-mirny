267c950be6101e482044e75323ea98f43422404a
GLiese-S <oleg-sultan@rambler.ru>
2022-07-23 22:58:39 +0800

    feat (add nodemon)

---
+++ test-back/server.js
@@ -0,0 +1,12 @@
+
+import express from "express";
+
+const PORT = 5000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).json('Сервер запущен');
+})
+
+app.listen(PORT, () => console.log('SERVER STARTED ON PORT ' + PORT));
